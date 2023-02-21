7f59cb13bc44f5a5a185d936adb354df9a46a516
Juraj Bednar <juraj@bednar.io>
2023-02-21 23:17:21 +0100

    Make process_vtt.sh more universal

--- process_vtt.sh
+++ process_vtt.sh
@@ -5,8 +5,8 @@ for i in *.vtt
         echo "Processing ${i}"
         vtt2txt "${i}"
         textfile="${i%.vtt}.txt"
-        summaryfile="${i%.vtt}-summary.txt"
-        ./recursively_summarize.py "${textfile}" "${summaryfile}"
+        summaryfile="${i%.vtt}-summary.md"
+        $(dirname "$0")/recursively_summarize.py "${textfile}" "${summaryfile}"
     done
 
 
